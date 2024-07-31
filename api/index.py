c7dbd9bc98b974ae814734362a67ea0bc148edec
FenghenHome <s.sunfeihu@gmail.com>
2024-07-31 10:05:03 +0000

    添加变量
    Signed-off-by: FenghenHome <s.sunfeihu@gmail.com>

--- api/index.py
+++ api/index.py
@@ -1,3 +1,4 @@
+import os
 from itertools import islice
 
 from duckduckgo_search import DDGS
@@ -5,14 +6,19 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+# Retrieve SECRET_KEY and SAFESEARCH from environment variables
+SECRET_KEY = os.getenv('SECRET_KEY')
+SAFESEARCH = os.getenv('SAFESEARCH', 'ON')  # Default to 'ON' if not set
 
+# Print SECRET_KEY and SAFESEARCH (for debugging purposes only)
+print(f"SECRET_KEY: {SECRET_KEY}")
+print(f"SAFESEARCH: {SAFESEARCH}")
 def run():
     if request.method == 'POST':
         keywords = request.form['q']
         max_results = int(request.form.get('max_results', 10))
     else:
         keywords = request.args.get('q')
-        # 从请求参数中获取最大结果数，如果未指定，则默认为10
         max_results = int(request.args.get('max_results', 10))
     return keywords, max_results
 
@@ -22,13 +28,10 @@ async def search():
     keywords, max_results = run()
     results = []
     with DDGS() as ddgs:
-        # 使用DuckDuckGo搜索关键词
-        ddgs_gen = ddgs.text(keywords, safesearch='Off', timelimit='y', backend="lite")
-        # 从搜索结果中获取最大结果数
+        ddgs_gen = ddgs.text(keywords, safesearch=SAFESEARCH, timelimit='y', backend="lite")
         for r in islice(ddgs_gen, max_results):
             results.append(r)
 
-    # 返回一个json响应，包含搜索结果
     return {'results': results}
 
 
@@ -37,13 +40,10 @@ async def search_answers():
     keywords, max_results = run()
     results = []
     with DDGS() as ddgs:
-        # 使用DuckDuckGo搜索关键词
         ddgs_gen = ddgs.answers(keywords)
-        # 从搜索结果中获取最大结果数
         for r in islice(ddgs_gen, max_results):
             results.append(r)
 
-    # 返回一个json响应，包含搜索结果
     return {'results': results}
 
 
@@ -52,13 +52,10 @@ async def search_images():
     keywords, max_results = run()
     results = []
     with DDGS() as ddgs:
-        # 使用DuckDuckGo搜索关键词
-        ddgs_gen = ddgs.images(keywords, safesearch='Off', timelimit=None)
-        # 从搜索结果中获取最大结果数
+        ddgs_gen = ddgs.images(keywords, safesearch=SAFESEARCH, timelimit=None)
         for r in islice(ddgs_gen, max_results):
             results.append(r)
 
-    # 返回一个json响应，包含搜索结果
     return {'results': results}
 
 
@@ -67,13 +64,10 @@ async def search_videos():
     keywords, max_results = run()
     results = []
     with DDGS() as ddgs:
-        # 使用DuckDuckGo搜索关键词
-        ddgs_gen = ddgs.videos(keywords, safesearch='Off', timelimit=None, resolution="high")
-        # 从搜索结果中获取最大结果数
+        ddgs_gen = ddgs.videos(keywords, safesearch=SAFESEARCH, timelimit=None, resolution="high")
         for r in islice(ddgs_gen, max_results):
             results.append(r)
 
-    # 返回一个json响应，包含搜索结果
     return {'results': results}
 
 
